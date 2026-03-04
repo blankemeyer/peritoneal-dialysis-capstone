cc9c2eee99bce9c0907332f6a9a7b1c5a8720243
blankemeyer <blankemeyer.33@osu.edu>
2026-03-04 13:46:38 -0500

    create IInput interface and PWMOutput class
    PWMOutput: pass HAL by reference
    
    make PWMOutput derive from IOutput
    
    PWMOutput.cpp: add TODO about MAX_QUANT
    
    PWMOutput: fix couple typos
    
    PWMOutput: fix another typo :(

---
+++ lib/Output/PWMOutput.cpp
@@ -0,0 +1,20 @@
+#include "PWMOutput.h"
+
+#include <assert.h>
+
+// TODO: should be defined by IHAL method, e.g., a member function
+//       maxOutputQuant()
+#define MAX_QUANT 255
+
+PWMOutput::PWMOutput(IHAL &hal, int pin) : m_hal{hal}, m_pin{pin} {}
+
+void PWMOutput::setValue(float dutyCycle) {
+	// TODO: Should these be preconditions, or should we normalize
+	//       `dutyCycle` ourselves?
+	assert(dutyCycle >= 0.f);
+	assert(dutyCycle <= 1.f);
+
+	int quant = (int)(dutyCycle * (float)MAX_QUANT);
+
+	m_hal.analogWrite(m_pin, quant);
+}
