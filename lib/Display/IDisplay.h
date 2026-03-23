8426b2581b5657998afd9a86b162a19c7e8eba9e
Dennis Zhitenev <denniszhitenev@gmail.com>
2026-03-23 17:35:08 -0400

    Write IDisplay and SerialDisplay

--- lib/Display/IDisplay.h
+++ lib/Display/IDisplay.h
@@ -6,9 +6,25 @@
 #define IDISPLAY_H
 
 /**
- * Display Interface
- *
- * Has an update method that takes in values to be shown on the display.
+ * Interface for displaying important values related to the system. Currently it
+ * supports displaying the power level on each channel output.
  */
+class IDisplay {
+public:
+	virtual ~IDisplay() = default;
+
+	/**
+	 * Initialize the display. In general, must be called before any calls to
+	 * `update`.
+	 */
+	virtual void initialize() = 0;
+
+	/**
+	 * Update the display with all relevant values.
+	 * @param channel1Power The output value for channel 1 (between 0 and 1)
+	 * @param channel2Power The output value for channel 2 (between 0 and 1)
+	 */
+	virtual void update(float channel1Power, float channel2Power) = 0;
+};
 
 #endif // IDISPLAY_H
--- lib/Display/SerialDisplay.h
+++ lib/Display/SerialDisplay.h
@@ -1,15 +1,25 @@
 /**
- * @file ISerialDisplay.h
+ * @file SerialDisplay.h
  */
 
 #ifndef SERIAL_DISPLAY_H
 #define SERIAL_DISPLAY_H
 
+#include <IDisplay.h>
+#include <IHAL.h>
+
 /**
- * Serial display
- *
- * Implementation of `IDisplay` that uses the HAL to write text messages to
- * serial output.
+ * Implementation of `IDisplay` that uses the HAL to write to the serial output.
  */
+class SerialDisplay : public IDisplay {
+private:
+	IHAL &hal;
+	char string_buffer[128];
+
+public:
+	SerialDisplay(IHAL &hal);
+	void initialize() override;
+	void update(float channel1Power, float channel2Power) override;
+};
 
 #endif // SERIAL_DISPLAY_H
